f237c64d3802cc342449ccd5a92b321fb11fe0c6
OseidAldary(Joker11) <oseid.aldary@gmail.com>
2017-07-27 11:21:31 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "installing Urlsnarf "
+sleep 3
+
+chmod +x urlsnarf
+cp urlsnarf /usr/sbin/
+echo "Done Urlsnarf has been installed :) "
+sleep 1
+echo
+read -p "Press Enter to exit "
+clear
+exit
+
